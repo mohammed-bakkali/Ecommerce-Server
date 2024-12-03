8991bec10c8c45fcbf85184be705c4080de840d7
Mohammed <mohammedbakkali807@gmail.com>
2024-12-03 21:42:09 +0100

    Update

---
+++ models/sucCategoryModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: [true, "Category must be unique"],
+      minlength: [2, "To short SubCategory name"],
+      maxlength: [32, "To long SubCategory name"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      require: [true, "SubCategory must be belong to parent category"],
+    },
+  },
+  { timestamps: true }
+);
+// Converting a schema into a model
+module.exports = mongoose.model("SubCategory", subCategorySchema);
